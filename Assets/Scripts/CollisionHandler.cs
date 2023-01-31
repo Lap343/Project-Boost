f6a23ea73d47570a0d9efc82308896b0baf14ec9
Luis Perez <44172765+Lap343@users.noreply.github.com>
2023-01-30 19:20:03 -0800

    feat(collision): added debug logs to player to detect when and what they collided to

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -4,6 +4,20 @@ public class CollisionHandler : MonoBehaviour
 {
     void OnCollisionEnter(Collision other) 
     {
-        
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("Well excuuuuuse me princess!");
+                break;
+            case "Finish":
+                Debug.Log("YOU WON!!!");
+                break;
+            case "Fuel":
+                Debug.Log("You picked up fule!");
+                break;
+            default:
+                Debug.Log("YOU DIED!!!");
+                break;
+        }
     }
 }
